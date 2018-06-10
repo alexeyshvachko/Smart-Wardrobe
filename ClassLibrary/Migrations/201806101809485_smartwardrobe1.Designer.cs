8787403a71ac8236fc3de6778dea179c5b94d62a
Швачко Алексей Игоревич <aishvachko@edu.hse.ru>
2018-06-10 21:10:45 +0300

    4

--- ClassLibrary/Migrations/201806101809485_smartwardrobe1.Designer.cs
+++ ClassLibrary/Migrations/201806101809485_smartwardrobe1.Designer.cs
@@ -7,13 +7,13 @@ namespace Classes.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class firstwardrobe : IMigrationMetadata
+    public sealed partial class smartwardrobe1 : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(firstwardrobe));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(smartwardrobe1));
         
         string IMigrationMetadata.Id
         {
-            get { return "201806101750473_firstwardrobe"; }
+            get { return "201806101809485_smartwardrobe1"; }
         }
         
         string IMigrationMetadata.Source
